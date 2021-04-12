12db6f5c6986a1330d2da8cf19479cf2ba5c47cb
TomasUwU <tomas.saglietti@davinci.edu.ar>
2021-04-11 22:46:36 -0300

    Movimiento Guerrero
    Movimiento y salto guerrero. Cámara provisoria.

---
+++ ProjectSky/Assets/Scripts/MovimientoPJ.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MovimientoPJ : MonoBehaviour
+{
+    public float _velocidad = 6f;
+    public float _rotationSpeed;
+    public float _fuerzaSalto = 7f;
+
+    public Rigidbody rb;
+
+    public LayerMask groundLayers; //Cambiar si se cambian las layers del piso
+
+    public CapsuleCollider col;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+        WarriorMovement();
+        Jump();
+    }
+
+    void WarriorMovement()
+    {
+        //Movimiento
+        float _horizontal = Input.GetAxisRaw("Horizontal");
+        float _vertical = Input.GetAxisRaw("Vertical");
+        Vector3 playerMovement = new Vector3(_horizontal, 0f, _vertical);
+        transform.Translate(playerMovement * _velocidad * Time.deltaTime, Space.World);
+
+        //Rotacion
+        if (playerMovement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(playerMovement, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    void Jump()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * _fuerzaSalto, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+}
