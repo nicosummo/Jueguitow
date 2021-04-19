61fd56d71ebeee40d028cbc66db90885db7ad88a
aldyfairy <69333949+aldyfairy@users.noreply.github.com>
2021-04-19 19:52:41 -0300

    inicio

---
+++ ProjectSky/Assets/Scripts/Menu/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+       public void cargaLevel (string nombreNIvel)
+    {
+        SceneManager.LoadScene (nombreNIvel);
+    }
+}
