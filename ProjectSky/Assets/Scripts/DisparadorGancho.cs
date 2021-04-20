5cff658588929bbbf8f6be80ed57d85cbb933d9d
Nico Summo <ngsummo@yahoo.com.ar>
2021-04-20 13:16:27 -0300

    Gancho

---
+++ ProjectSky/Assets/Scripts/DisparadorGancho.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparadorGancho : MonoBehaviour
+{
+    [SerializeField] float pullSpeed = 0.5f;
+    [SerializeField] float stopDistance = 4f;
+    [SerializeField] GameObject hookPrefab;
+    [SerializeField] Transform shootTransform;
+
+    Gancho gancho;
+    bool pulling;
+    Rigidbody rb;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pulling = false;
+        
+    }
+
+    
+    void Update()
+    {
+        if(gancho == null && Input.GetMouseButtonDown(0))
+        {
+            StopAllCoroutines();
+            pulling = false;
+            gancho = Instantiate(hookPrefab, shootTransform.position, Quaternion.identity).GetComponent<Gancho>();
+            gancho.Initialize(this, shootTransform);
+
+            StartCoroutine(DestroyHookAfterLifetime());
+        }
+        else if (gancho != null && Input.GetMouseButtonDown(1))
+        {
+            DestroyHook();
+        }
+
+        if (!pulling || gancho == null) return;
+
+        if(Vector3.Distance(transform.position, gancho.transform.position) <= stopDistance)
+        {
+            DestroyHook();
+        }
+        else
+        {
+            rb.AddForce((gancho.transform.position - transform.position).normalized * pullSpeed, ForceMode.VelocityChange);
+        }
+    }
+
+    public void StartPull()
+    {
+        pulling = true;
+    }
+
+    private void DestroyHook()
+    {
+        if (gancho == null) return;
+
+        pulling = false;
+        Destroy(gancho.gameObject);
+        gancho = null;
+    }
+
+    private IEnumerator DestroyHookAfterLifetime()
+    {
+        yield return new WaitForSeconds(8f);
+
+        DestroyHook();
+    }
+}
---
+++ ProjectSky/Assets/Scripts/Gancho.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gancho : MonoBehaviour
+{
+    [SerializeField] float hookForce = 25f;
+
+    DisparadorGancho grapple;
+    Rigidbody rb;
+    LineRenderer lineRenderer;
+    public void Initialize(DisparadorGancho grapple, Transform shootTransform)
+    {
+        transform.forward = shootTransform.forward;
+        this.grapple = grapple;
+        rb = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+        rb.AddForce(transform.forward * hookForce, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        Vector3[] positions = new Vector3[]
+        {
+            transform.position, grapple.transform.position
+        };
+
+        lineRenderer.SetPositions(positions);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((LayerMask.GetMask("Gancho") & 1 << other.gameObject.layer) > 0)
+        {
+            rb.useGravity = false;
+            rb.isKinematic = true;
+
+            grapple.StartPull();
+        }
+    }
+}
