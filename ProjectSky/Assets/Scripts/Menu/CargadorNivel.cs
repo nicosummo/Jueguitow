d6990dbb61300d7f3d69cab4ffe1744c69fca48f
aldyfairy <69333949+aldyfairy@users.noreply.github.com>
2021-04-21 23:53:15 -0300

    Loading Completo

---
+++ ProjectSky/Assets/Scripts/Menu/CargadorNivel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CargadorNivel : MonoBehaviour
+{
+    public GameObject PantallaDeCarga;
+    public Slider Slider;
+
+
+    public void CargarNivel(int NumeroDeEscena)
+    {
+        StartCoroutine(CargarAsync(NumeroDeEscena));
+        
+    }
+
+    IEnumerator CargarAsync (int NumeroDeEscena)
+    {
+        AsyncOperation Operacion = SceneManager.LoadSceneAsync(NumeroDeEscena);
+
+        PantallaDeCarga.SetActive(true);
+
+        while (!Operacion.isDone)
+	    {
+            float Progreso = Mathf.Clamp01(Operacion.progress / .9f);
+            Slider.value = Progreso;  
+
+            yield return null;
+        
+        }
+    }
+
+}
